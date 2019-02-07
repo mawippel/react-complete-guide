46d443493373591db9a70d7adf929b58b00e4046
Marcelo Wippel <TECBMMWL@TECLOGICA.COM.BR>
2019-02-07 09:46:14 -0200

    Initial setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-complete-guide
 Udemy react.js course.
+
+link: https://www.udemy.com/react-the-complete-guide-incl-redux/
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <h1 className="App">
+        Hi, i'm a new react.js app!
+      </h1>
+    );
+  } 
+}
+
+export default App;
