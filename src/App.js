deda8501697365266dcacc4dd77ef74a40c38062
mawippel <mawippel2@hotmail.com>
2019-02-07 14:43:40 -0200

    state, props and componentization

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      {name: 'Marcelo', age: 20},
+      {name: 'Naiara', age: 21},
+      {name: 'Merini', age: 20}
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        {name: 'Marcelo', age: 21},
+        {name: 'Nai', age: 21},
+        {name: 'Merinex', age: 20}
+      ]
+    });
+  }
+
   render() {
     return (
-      <h1 className="App">
-        Hi, i'm a new react.js app!
-      </h1>
+      <div className="App">
+        <h1>Hi, i'm a new react.js app!</h1>
+        <button onClick={this.switchNameHandler}>Switch Name!</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Studying</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      </div>
     );
   } 
 }
