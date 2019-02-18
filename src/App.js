715e0486dc815ccbe7cf4a8fef552e5161a759d9
mawippel <mawippel2@hotmail.com>
2019-02-18 11:11:35 -0300

    lists and styling

--- src/App.js
+++ src/App.js
@@ -6,35 +6,31 @@ class App extends Component {
 
   state = {
     persons: [
-      { name: 'Marcelo', age: 20 },
-      { name: 'Naiara', age: 21 },
-      { name: 'Merini', age: 20 }
+      { id: '1', name: 'Marcelo', age: 20 },
+      { id: '2', name: 'Naiara', age: 21 },
+      { id: '3', name: 'Merini', age: 20 }
     ],
     showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    this.setState({
-      persons: [
-        { name: newName, age: 21 },
-        { name: 'Nai', age: 21 },
-        { name: 'Merinex', age: 20 }
-      ]
+  inputNameChangeHandler = (event, id) => {
+    let index = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
-  }
 
-  inputNameChangeHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Marcelo!', age: 21 },
-        { name: event.target.value, age: 21 },
-        { name: 'Merinex', age: 20 }
-      ]
-    });
+    let copyPerson = {
+      ...this.state.persons[index]
+    };
+    copyPerson.name = event.target.value;
+
+    let newPersons = [...this.state.persons];
+    newPersons[index] = copyPerson;
+    this.setState({ persons: newPersons });
   }
 
   deletePersonHandler = (index) => {
-    let persons = this.state.persons;
+    //let persons = this.state.persons.slice(); --old way
+    let persons = [...this.state.persons] //ES6 way, with spread operator
     persons.splice(index, 1);
     this.setState({ persons: persons });
   }
@@ -44,39 +40,49 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+      marginBottom: '8px'
+    }
 
-    let personList: null;
+    let personList = null;
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red'); // add red class
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold'); // add bold class
+    }
 
     if (this.state.showPersons) {
       personList = (
         <div>
           {this.state.persons.map((person, i) => {
-            return <Person key={i}
+            return <Person key={person.id}
               clickHandler={() => this.deletePersonHandler(i)}
               name={person.name}
-              age={person.name} />
+              age={person.age}
+              inputHandler={(event) => this.inputNameChangeHandler(event, person.id)} />
           })}
         </div>
       )
+
+      style.backgroundColor = 'red';
     }
 
     return (
-      <div className="App">
-        <h1>Hi, i'm the brand new Marcelo's react.js app!</h1>
-        <button style={style} onClick={this.toggleShowPersons}>Show Persons</button>
-        {personList}
-      </div>
+        <div className="App">
+          <p className={classes.join(' ')}>Hi, i'm the brand new Marcelo's react.js app!</p>
+          <button style={style} onClick={this.toggleShowPersons}>Show Persons</button>
+          {personList}
+        </div>
     );
   }
-}
-
-const style = {
-  backgroundColor: 'white',
-  font: 'inherit',
-  border: '1px solid blue',
-  padding: '8px',
-  cursor: 'pointer',
-  marginBottom: '8px'
 }
 
 export default App;
